c5c4085bb36ccfe406a0757841c49c3842177873
Praveenkusuluri08 <praveenkusuluri08@gmail.com>
2022-04-30 14:31:52 +0530

    Updated authorization headers and token with bearer

--- Backend/helpers/endPoint.go
+++ Backend/helpers/endPoint.go
@@ -1,21 +1,25 @@
 package helpers
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func EndPoint() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.Request.Header.Get("token")
-
-		if token == "" {
+		token := c.Request.Header.Get("Authorization")
+		bearerToken := strings.SplitAfter(token, "Bearer")
+		fmt.Println(bearerToken[1])
+		actualToken := strings.Replace(bearerToken[1], " ", "", -1)
+		if actualToken == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Token not found"})
 			c.Abort()
 			return
 		}
-		claims, err := ValidateJwtAuthToken(token)
+		claims, err := ValidateJwtAuthToken(actualToken)
 		if err != "" {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 
